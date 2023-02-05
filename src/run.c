1579dd903fc478aad0890b5237ea04224dd2b33c
adhfmz7 <ahmadaldasouqi559@gmail.com>
2023-02-05 08:35:03 -0800

    add process forking

--- src/run.c
+++ src/run.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include "term.h"
+#include <unistd.h>
 
 char **parse_line(char* line)
-{
-		
+{		
+
+
 	
+}
 
 
+int launch_process(char **args)
+{
+
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		printf("Failed to fork\n");
+		return -1;
+	}
+	else if (pid == 0)
+	{
+		pid = getpid();
+
+		return 0;
+	}
+	else
+	{
+		printf("child's pid is %d\n", pid);
+		printf("Parent's pid is potentially %d\n", getpid());
+	}
+
+	return (int) pid;
 }
 
 
@@ -16,6 +42,9 @@ int run(char *buffer)
 
 	char **args = parse_line(buffer);
 
+	
+	// we need to run the program with the proper arguments.
 
-	return -1;
+	return launch_process(args);
 }
+
